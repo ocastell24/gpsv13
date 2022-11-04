d43c314ce01ea86a3a2f6e5ad7a3e6d955169015
ocastell24 <ocastell@hotmail.com>
2022-11-03 23:20:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import { I18n } from 'aws-amplify';
 import logo from './logo.svg';
 import './App.css';
+import  {Amplify}   from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import React from 'react'
+import {withAuthenticator} from 'aws-amplify/ui-react';
+//import {withAuthenticator} from 'aws-amplify';
+Amplify.configure(awsExports);
 
-function App() {
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p> hola </p>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
